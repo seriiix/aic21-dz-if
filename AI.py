a532594dc101eb59a1586fc214e65d2b1f7068a3
Amir Hossein Jeddi <seriousmvs@gmail.com>
2021-04-24 02:46:56 +0430

    Add DEBUG code

--- AI.py
+++ AI.py
@@ -5,11 +5,22 @@ from typing import *
 from random import randint
 from x_env import Env
 
+
 class Brain:
     def __init__(self):
         self.env = Env()
         self.ant_id = randint(0, 2 ^ 12 - 1)
         self.ant_turn_number = 0
+
+        # FOR DEBUG ONLY
+        self.DEBUG = False
+        self.DEBUG_MOVES = [Direction.LEFT, Direction.DOWN, Direction.DOWN,
+                            Direction.DOWN, Direction.DOWN, Direction.DOWN, Direction.DOWN, Direction.DOWN, Direction.DOWN, Direction.DOWN]
+
+    def get_debug_move(self) -> Direction:
+        if self.ant_turn_number <= len(self.DEBUG_MOVES):
+            return self.DEBUG_MOVES[self.ant_turn_number - 1]
+        return Direction.CENTER
 
 
 brain = Brain()
@@ -30,8 +41,10 @@ class AI:
         # try:
         message, value, direction = brain.env.run_one_turn(brain.ant_id)
         # except Exception as e:
-            # message, value, direction = str(e) if len(str(e))<32 else str(e)[0:31], 103, 0
-            # print(e)
-            # return (message,value, direction)
+        # message, value, direction = str(e) if len(str(e))<32 else str(e)[0:31], 103, 0
+        # print(e)
+        # return (message,value, direction)
+        if brain.DEBUG:
+            return (message, value, brain.get_debug_move().value)
 
         return (message, value, direction)
